608e5cf218e9ffd940e46be0ebb1b59ca4d77a80
hirakiuc <hirakiuc@gmail.com>
2019-08-12 12:38:19 +0900

    Initial import(WIP)

---
+++ cmd/jira/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+
+	aw "github.com/deanishe/awgo"
+	"github.com/hirakiuc/alfred-jira-workflow/cli"
+)
+
+// Your workflow starts here
+func run(wf *aw.Workflow) func() {
+	return func() {
+		cmd := cli.ParseArgs(wf.Args())
+		ctx := context.Background()
+
+		cmd.Run(ctx, wf)
+
+		// Add a "Script Filter" result
+		// wf.NewItem("First result!")
+
+		// Send results to Alfred
+		wf.SendFeedback()
+	}
+}
+
+func main() {
+	// Create a new Workflow using default settings.
+	// Critical settings are provided by Alfred via environment variables.
+	// so this *will* die in flames if not run in an Alfred-like environment.
+	wf := aw.New()
+
+	// Wrap your entry point with Run() to catch and log panics and
+	// show an error in Alfred instead of silently dying
+	wf.Run(run(wf))
+}
---
+++ subcommand/subcommand.go
@@ -0,0 +1,32 @@
+package subcommand
+
+import (
+	"context"
+	"strings"
+
+	aw "github.com/deanishe/awgo"
+)
+
+type SubCommand interface {
+	// Run the subcommand
+	Run(ctx context.Context, wf *aw.Workflow)
+
+	// Get the arguments
+	Arguments() []string
+}
+
+type BaseCommand struct {
+	Args []string
+}
+
+func (cmd BaseCommand) Arguments() []string {
+	return cmd.Args
+}
+
+func (cmd BaseCommand) HasQuery() bool {
+	return len(cmd.Args) > 0
+}
+
+func (cmd BaseCommand) Query() string {
+	return strings.Join(cmd.Args, " ")
+}
