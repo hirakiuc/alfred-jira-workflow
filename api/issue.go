19ac146d56e533996a65d974478871904414d065
hirakiuc <hirakiuc@gmail.com>
2019-10-05 15:46:45 +0900

    Add IssueResource

--- api/issue.go
+++ api/issue.go
@@ -1,31 +1,21 @@
 package api
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
 	"github.com/andygrunwald/go-jira"
 )
 
-func (client *Client) SearchIssues(args []string) ([]jira.Issue, error) {
+func (client *Client) SearchIssues(jql string) ([]jira.Issue, error) {
 	opts := jira.SearchOptions{
 		StartAt:    0,
 		MaxResults: 200,
 	}
 
-	jql := args2jql(args)
-	fmt.Fprintf(os.Stderr, "jql:%s\n", jql)
 	issues, _, err := client.jira.Issue.Search(jql, &opts)
 	if err != nil {
 		return []jira.Issue{}, err
 	}
 
 	return issues, nil
-}
-
-func args2jql(args []string) string {
-	return strings.Join(args, " AND ")
 }
 
 func (client *Client) GetIssue(issueID string) (*jira.Issue, error) {
