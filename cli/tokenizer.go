01ccd2a36a3e9a2ddd1e9cb50de1739548763a16
hirakiuc <hirakiuc@gmail.com>
2019-10-06 15:03:51 +0900

    Use 1st arg as jql

--- cli/tokenizer.go
+++ cli/tokenizer.go
@@ -25,19 +25,21 @@ func NewTokenizer() *Tokenizer {
 func normalizeTokens(args []string) ([]string, error) {
 	ret := []string{}
 
-	parser := shellwords.NewParser()
-	// ["a b c d"] or ["a", "b", "c", "d"]
-	for _, component := range args {
-		parts, err := parser.Parse(component)
+	// Case: ["a b c d"] or ["a"]
+	if len(args) == 1 {
+		parser := shellwords.NewParser()
+		parts, err := parser.Parse(args[0])
 		if err != nil {
 			return []string{}, err
 		}
+		return parts, nil
+	}
 
-		for _, arg := range parts {
-			v := strings.TrimSpace(arg)
-			if v != "" {
-				ret = append(ret, v)
-			}
+	// Case: ["a", "b", "c", "d"] (from cli)
+	for _, token := range args {
+		v := strings.TrimSpace(token)
+		if v != "" {
+			ret = append(ret, v)
 		}
 	}
 
@@ -49,6 +51,7 @@ func (t *Tokenizer) Tokenize(args []string) error {
 	if err != nil {
 		return err
 	}
+
 	t.tokens = tokens
 	t.pos = 0
 
