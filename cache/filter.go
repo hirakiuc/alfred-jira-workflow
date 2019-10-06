274c8a3e469a988dc8f02169ce93ac68af3e4d4a
hirakiuc <hirakiuc@gmail.com>
2019-10-06 16:41:11 +0900

    Implement my-filter issues

---
+++ cache/filter.go
@@ -0,0 +1,91 @@
+package cache
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/andygrunwald/go-jira"
+	aw "github.com/deanishe/awgo"
+)
+
+type FiltersCache struct {
+	*BaseCache
+}
+
+func NewFiltersCache(wf *aw.Workflow) *FiltersCache {
+	return &FiltersCache{
+		&BaseCache{
+			wf: wf,
+		},
+	}
+}
+
+func (cache *FiltersCache) getCacheWithKey(cacheKey string) ([]jira.Filter, error) {
+	filters := []jira.Filter{}
+	err := cache.getRawCache(cacheKey, getMaxCacheAge(), &filters)
+	if err != nil {
+		return []jira.Filter{}, err
+	}
+	if filters == nil {
+		return []jira.Filter{}, nil
+	}
+
+	return filters, nil
+}
+
+func (cache *FiltersCache) storeWithKey(cacheKey string, filters []jira.Filter) ([]jira.Filter, error) {
+	_, err := cache.storeRawData(cacheKey, filters)
+	if err != nil {
+		return filters, err
+	}
+
+	return filters, nil
+}
+
+//----------------------------------------------------------
+func (cache *FiltersCache) getCacheKey() string {
+	return "filters"
+}
+
+func (cache *FiltersCache) GetFiltersCache() ([]jira.Filter, error) {
+	cacheKey := cache.getCacheKey()
+	return cache.getCacheWithKey(cacheKey)
+}
+
+func (cache *FiltersCache) StoreFilters(filters []jira.Filter) ([]jira.Filter, error) {
+	cacheKey := cache.getCacheKey()
+	return cache.storeWithKey(cacheKey, filters)
+}
+
+//----------------------------------------------------------
+func (cache *FiltersCache) getCacheKeyWithID(filterID int) string {
+	return fmt.Sprintf("filter-%d", filterID)
+}
+
+func (cache *FiltersCache) GetFilterCache(filterID int) (*jira.Filter, error) {
+	cacheKey := cache.getCacheKeyWithID(filterID)
+	filters, err := cache.getCacheWithKey(cacheKey)
+	if err != nil {
+		return nil, err
+	}
+	if len(filters) == 0 {
+		return nil, nil
+	}
+
+	return &filters[0], nil
+}
+
+func (cache *FiltersCache) StoreFilter(filter *jira.Filter) (*jira.Filter, error) {
+	strID, err := strconv.Atoi(filter.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	cacheKey := cache.getCacheKeyWithID(strID)
+	filters, err := cache.storeWithKey(cacheKey, []jira.Filter{*filter})
+	if err != nil {
+		return nil, err
+	}
+
+	return &filters[0], nil
+}
--- subcommand/my-filter.go
+++ subcommand/my-filter.go
@@ -2,7 +2,10 @@ package subcommand
 
 import (
 	"context"
+	"strconv"
+	"strings"
 
+	"github.com/andygrunwald/go-jira"
 	aw "github.com/deanishe/awgo"
 	"github.com/hirakiuc/alfred-jira-workflow/decorator"
 	"github.com/hirakiuc/alfred-jira-workflow/resource"
@@ -20,7 +23,7 @@ func NewMyFilterCommand(args []string) MyFilterCommand {
 	}
 }
 
-func (cmd MyFilterCommand) Run(ctx context.Context, wf *aw.Workflow) {
+func (cmd MyFilterCommand) showMyFilters(ctx context.Context, wf *aw.Workflow, opts []string) {
 	r := resource.NewFilterResource(wf)
 	filters, err := r.MyFilters(ctx)
 	if err != nil {
@@ -44,10 +47,73 @@ func (cmd MyFilterCommand) Run(ctx context.Context, wf *aw.Workflow) {
 			Valid(true)
 	}
 
-	if cmd.HasQuery() {
-		wf.Filter(cmd.Query())
+	if len(opts) > 0 {
+		wf.Filter(strings.Join(opts, " "))
 	}
 
 	// Show a warning in alfred if there are no filters.
 	wf.WarnEmpty("No filters found.", "")
+}
+
+func (cmd MyFilterCommand) showFilteredIssues(_ context.Context, wf *aw.Workflow, filter *jira.Filter, opts []string) {
+	r := resource.NewIssueResource(wf)
+	issues, err := r.SearchIssues([]string{filter.Jql})
+	if err != nil {
+		wf.FatalError(err)
+		return
+	}
+
+	d, err := decorator.NewIssueDecorator(wf)
+	if err != nil {
+		wf.FatalError(err)
+		return
+	}
+
+	for _, issue := range issues {
+		v := issue
+		d.SetTarget(&v)
+
+		wf.NewItem(d.Title()).
+			Arg(d.URL()).
+			Valid(true)
+	}
+
+	if len(opts) > 0 {
+		wf.Filter(strings.Join(opts, " "))
+	}
+
+	// Show a warning in alfred if there are no filters.
+	wf.WarnEmpty("No issues found.", "")
+}
+
+func (cmd MyFilterCommand) Run(ctx context.Context, wf *aw.Workflow) {
+	if len(cmd.Args) == 0 {
+		// case:my-filter
+		cmd.showMyFilters(ctx, wf, cmd.Args)
+		return
+	}
+
+	firstArg := cmd.Args[0]
+	opts := []string{}
+	if len(cmd.Args) > 1 {
+		opts = cmd.Args[1:]
+	}
+
+	filterID, err := strconv.Atoi(firstArg)
+	if err != nil {
+		// case:my-filter [word] ...
+		cmd.showMyFilters(ctx, wf, cmd.Args)
+		return
+	}
+
+	r := resource.NewFilterResource(wf)
+	filter, err := r.GetFilterByID(filterID)
+	if err != nil || filter == nil {
+		// case:my-filter [word]... No such filter found
+		cmd.showMyFilters(ctx, wf, cmd.Args)
+		return
+	}
+
+	// Show filtered issues
+	cmd.showFilteredIssues(ctx, wf, filter, opts)
 }
