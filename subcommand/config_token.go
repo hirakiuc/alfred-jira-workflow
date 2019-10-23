a2e1f2a04c13405f2746d48a398c4b0cce592164
hirakiuc <hirakiuc@gmail.com>
2019-10-24 00:16:56 +0900

    Add config token subcommand

---
+++ subcommand/config_token.go
@@ -0,0 +1,55 @@
+package subcommand
+
+import (
+	"context"
+	"fmt"
+
+	aw "github.com/deanishe/awgo"
+	"github.com/hirakiuc/alfred-jira-workflow/secret"
+)
+
+// ConfigTokenCommand describe a subcommand to configure the api token.
+type ConfigTokenCommand struct {
+	Token string
+
+	BaseCommand
+}
+
+// NewConfigTokenCommand return an instance of ConfigTokenCommand
+func NewConfigTokenCommand(token string) ConfigTokenCommand {
+	return ConfigTokenCommand{
+		Token: token,
+
+		BaseCommand: BaseCommand{
+			Args: []string{},
+		},
+	}
+}
+
+// Run start this subcommand
+func (cmd ConfigTokenCommand) Run(ctx context.Context, wf *aw.Workflow) {
+	store := secret.NewStore(wf)
+
+	if len(cmd.Token) == 0 {
+		token, err := store.GetAPIToken()
+		if err != nil {
+			wf.FatalError(err)
+			return
+		}
+		if len(token) == 0 {
+			wf.WarnEmpty("No token found.", "")
+			return
+		}
+
+		wf.NewItem(fmt.Sprintf("Found token:%s", token))
+		return
+	}
+
+	err := store.Store(secret.KeyJIRAToken, cmd.Token)
+	if err != nil {
+		wf.FatalError(err)
+		return
+	}
+
+	wf.NewItem("Success.")
+}
